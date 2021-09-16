9dde5d9d0d5171fcb2be7bdd6725a489912f7cbf
Sebastien Jerczynski <Sebastien.Jerczynski@bentley.com>
2021-09-16 15:32:50 -0400

    Continued code + added calculation of width of propagated pulse

--- dispersion.py
+++ dispersion.py
@@ -8,14 +8,15 @@ class Pulse:
         self.duration = duration
         self.TL = TL
         self.wavelength = wavelength
-        self.time = np.linspace(-1000e-15, 1000e-15, 4000)
-        self.IntensityTime = 1/((self.duration / 2.35482)*np.sqrt(2*np.pi))*np.exp(-self.time**2 / (2*(self.duration / self.TL / 2.35482)**2))
-        self.EfieldTime = np.sqrt(self.IntensityTime)
+        self.time = np.linspace(-5000e-15, 5000e-15, 5000)
+        self.IntensityTime = 1/((self.duration / 2.35482)*np.sqrt(2*np.pi))*np.exp(-self.time**2 / (2*(self.duration / 2.35482)**2))
+        self.IntensityTimeTL = 1 / ((self.duration / 2.35482) * np.sqrt(2 * np.pi)) * np.exp(-self.time ** 2 / (2 * (self.duration / self.TL / 2.35482) ** 2))
+        self.EfieldTime = np.sqrt(self.IntensityTimeTL)
 
 
     @property
     def frequencySpectra(self):
-        self.EfieldFrequency = np.abs(np.fft.fftshift(np.fft.fft(self.EfieldTime)))
+        self.EfieldFrequency = np.fft.fftshift(np.fft.fft(self.EfieldTime))
         self.IntensityFrequency = self.EfieldFrequency**2
         self.frequency  = np.fft.fftshift(np.fft.fftfreq(self.EfieldFrequency.shape[0],self.time[1]-self.time[0]))
         self.FWHMfreq = 2 * np.abs(self.frequency[np.argmin(np.abs(0.5*np.max(self.IntensityFrequency)-self.IntensityFrequency))])
@@ -23,22 +24,32 @@ class Pulse:
         return self.frequency, self.EfieldFrequency, self.IntensityFrequency, self.FWHMfreq, self.FWHMwavelength
 
 
-    def propagate(self, material:None, thickness):
+    def propagate(self, material, thickness):
         c = 299792458
         shiftedFreq = [i + c/self.wavelength for i in self.frequency]
         wavelengths = [c*10**6/j for j in shiftedFreq]
 
-        print(shiftedFreq)
+        x = Symbol('x')
+        if material == "silica":
+            n = (1 + 1.03961212 / (1 - 0.00600069867 / x ** 2) + 0.231792344 / (1 - 0.0200179144 / x ** 2) + 1.01046945 / (1 - 103.560653 / x ** 2))**.5
+        elif material == "BK7":
+            n = 1.2
+        else:
+            raise ValueError("chosen material not available")
 
-        # x = Symbol('x')
-        # f = (1 + 1.03961212 / (1 - 0.00600069867 / x ** 2) + 0.231792344 / (1 - 0.0200179144 / x ** 2) + 1.01046945 / (1 - 103.560653 / x ** 2))**.5
+        n = lambdify(x, n)  #input is in micrometers
+        phaseTerm = [exp(i*n(c*10**6/i)*thickness*-1j/c) for i in shiftedFreq]
+        EfieldFrequencyPropagated = [a*b for a,b in zip(self.EfieldFrequency, phaseTerm)]
+        EfieldTimeOut = abs(np.fft.ifft(np.fft.ifftshift(EfieldFrequencyPropagated)))
+        IntensityOut = EfieldTimeOut**2
+        mu = self.time.dot(IntensityOut / IntensityOut.sum())
+        mu2 = np.power(self.time, 2).dot(IntensityOut / IntensityOut.sum())
+        var = mu2 - mu ** 2
+        print("FWHM of new pulse : " + str(2.35482 * sqrt(var) * 10 ** 15))
+        plt.plot(self.time, IntensityOut, '-r', self.time, self.IntensityTime, '.b')
+
+        plt.show()
         # GVD = (self.wavelength*10**6)**3/(2*np.pi*(c)**2)*f.diff(x, 2) * 10**21
-        # GVD = lambdify(x, GVD)  #input is in micrometers
-        # EfieldTimeOut = np.fft.fftshift(np.abs(np.fft.ifft(np.fft.ifftshift(Eout))))
-        #plt.xlim((self.wavelength-6*FWHMwavelength)*10**6, (self.wavelength+6*FWHMwavelength)*10**6)
-        #plt.ylim(0.9, 1.8)
-        #plt.show()
-
 
     def plotPulses(self):
         fig, axs = plt.subplots(2,1, figsize=(6,8))
@@ -70,7 +81,7 @@ class Pulse:
         plt.savefig('time-bandwidth-product.png', dpi=200)
         plt.show()
 
-pulseTest = Pulse(120e-15, 1, 1050e-9)
+pulseTest = Pulse(90e-15, 1.13, 800e-9)
 pulseTest.frequencySpectra
-pulseTest.propagate("silica", 0.02)
-pulseTest.plotPulses()
+pulseTest.propagate("BK7", 0.15)
+#pulseTest.plotPulses()
