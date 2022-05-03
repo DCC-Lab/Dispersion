644cd3a0e82eea33d82b1f239cd576f8a1fe2475
Daniel <dccote@cervo.ulaval.ca>
2022-05-03 16:07:27 -0400

    Update pulse.py

--- pulse.py
+++ pulse.py
@@ -124,16 +124,31 @@ class Pulse:
 
         return self.time, field
 
-    def setupPlot(self, title=""):
+    def beginPlot(self, title=""):
         plt.style.use(
             "https://raw.githubusercontent.com/dccote/Enseignement/master/SRC/dccote-errorbars.mplstyle"
         )
         plt.title(title)
         plt.xlabel("Time [ps]")
         plt.ylabel("Field amplitude [arb.u.]")
-        plt.ylim(-1, 1)
 
-        axis = plt.gca()
+    def endPlot(self):
+        plt.show()
+
+    def beginPulse(self, axis=None):
+        if axis is None:
+            axis = plt.gca()
+
+        title = axis.get_title()
+        xlabel = axis.get_xlabel()
+        ylabel = axis.get_ylabel()
+
+        axis.clear()
+
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+
         axis.text(
             0.05,
             0.95,
@@ -147,8 +162,26 @@ class Pulse:
             verticalalignment="top",
         )
 
-    def tearDownPlot(self):
-        plt.clf()
+    def endPulse(self, filenameTemplate):
+        if filenameTemplate is not None:
+            plt.savefig(filenameTemplate.format(j), dpi=300)
+
+    def draw(self, axis=None, showChirp=True, showCarrier=False, adjustTimeScale=True, filenameTemplate=None):
+        pulse.beginPulse(axis)
+        pulse.drawEnvelope()
+        pulse.drawChirpColour()
+    
+        if showCarrier:
+            pulse.drawField()
+            plt.ylim(-1,1)
+
+        if adjustTimeScale:
+            𝛕 = pulse.temporalWidth*1e12
+            plt.xlim(-5*𝛕, 5*𝛕)
+        
+        plt.draw()
+        plt.pause(0.001)
+        pulse.endPulse(filenameTemplate)
 
     def drawEnvelope(self, axis=None):
         if axis is None:
@@ -202,83 +235,212 @@ class Pulse:
                 Polygon([(t1, 0), (t1, e1), (t2, e2), (t2, 0)], facecolor=hsv(c))
             )
 
-    def silica(self, wavelength):
-        x = wavelength * 1e6
-        if x < 0.3:
-            x = 0.3
-        elif x > 2.5:
-            x = 2.5
-        return (
-            1
-            + 1.03961212 / (1 - 0.00600069867 / x**2)
-            + 0.231792344 / (1 - 0.0200179144 / x**2)
-            + 1.01046945 / (1 - 103.560653 / x**2)
-        ) ** 0.5
+def silica(wavelength):
+    x = wavelength * 1e6
+    if x < 0.3:
+        x = 0.3
+    elif x > 2.5:
+        x = 2.5
+    return (
+        1
+        + 1.03961212 / (1 - 0.00600069867 / x**2)
+        + 0.231792344 / (1 - 0.0200179144 / x**2)
+        + 1.01046945 / (1 - 103.560653 / x**2)
+    ) ** 0.5
 
-    def bk7(self, wavelength):
-        x = wavelength * 1e6
-        if x < 0.3:
-            x = 0.3
-        elif x > 2.5:
-            x = 2.5
-        n = (
-            1
-            + 1.03961212 / (1 - 0.00600069867 / x**2)
-            + 0.231792344 / (1 - 0.0200179144 / x**2)
-            + 1.01046945 / (1 - 103.560653 / x**2)
-        ) ** 0.5
-        return n
+def bk7(wavelength):
+    x = wavelength * 1e6
+    if x < 0.3:
+        x = 0.3
+    elif x > 2.5:
+        x = 2.5
+    n = (
+        1
+        + 1.03961212 / (1 - 0.00600069867 / x**2)
+        + 0.231792344 / (1 - 0.0200179144 / x**2)
+        + 1.01046945 / (1 - 103.560653 / x**2)
+    ) ** 0.5
+    return n
 
-    def sf10(self, wavelength):
-        x = wavelength * 1e6
-        if x < 0.3:
-            x = 0.3
-        elif x > 2.5:
-            x = 2.5
-        return (
-            1
-            + 1.62153902 / (1 - 0.0122241457 / x**2)
-            + 0.256287842 / (1 - 0.0595736775 / x**2)
-            + 1.64447552 / (1 - 147.468793 / x**2)
-        ) ** 0.5
+def sf10(wavelength):
+    x = wavelength * 1e6
+    if x < 0.3:
+        x = 0.3
+    elif x > 2.5:
+        x = 2.5
+    return (
+        1
+        + 1.62153902 / (1 - 0.0122241457 / x**2)
+        + 0.256287842 / (1 - 0.0595736775 / x**2)
+        + 1.64447552 / (1 - 147.468793 / x**2)
+    ) ** 0.5
 
-    def water(self, wavelength):
-        x = wavelength * 1e6
-        if x < 0.3:
-            x = 0.3
-        elif x > 2.5:
-            x = 2.5
+def water(wavelength):
+    x = wavelength * 1e6
+    if x < 0.3:
+        x = 0.3
+    elif x > 2.5:
+        x = 2.5
 
-        x2 = x * x
-        n = (
-            1
-            + 5.672526103e-1 / (1 - 5.085550461e-3 / x2)
-            + 1.736581125e-1 / (1 - 1.814938654e-2 / x2)
-            + 2.121531502e-2 / (1 - 2.617260739e-2 / x2)
-            + 1.138493213e-1 / (1 - 1.073888649e1 / x2)
-        ) ** 0.5
-        return n
+    x2 = x * x
+    n = (
+        1
+        + 5.672526103e-1 / (1 - 5.085550461e-3 / x2)
+        + 1.736581125e-1 / (1 - 1.814938654e-2 / x2)
+        + 2.121531502e-2 / (1 - 2.617260739e-2 / x2)
+        + 1.138493213e-1 / (1 - 1.073888649e1 / x2)
+    ) ** 0.5
+    return n
 
+
+class Viewer:
+    def __init__(self, pulse, title):
+        self.pulse = pulse
+
+        self.figure = None
+        self.temporalAxis = None
+        self.spectralAxis = None
+        self.title = title
+        self.xlabel = "Time [ps]"
+        self.ylabel = "Field amplitude [arb.u.]"
+
+    def beginPlot(self):
+        self.figure = plt.subplot()
+
+        plt.style.use(
+            "https://raw.githubusercontent.com/dccote/Enseignement/master/SRC/dccote-errorbars.mplstyle"
+        )
+        plt.title(self.title)
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+
+    def endPlot(self):
+        plt.show()
+
+    def beginPulse(self, axis=None):
+        if axis is None:
+            axis = plt.gca()
+        else:
+            axis = self.temporalAxis
+
+        if axis is not None:
+            title = axis.get_title()
+            xlabel = axis.get_xlabel()
+            ylabel = axis.get_ylabel()
+            axis.clear()
+
+        plt.title(self.title)
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+
+        axis.text(
+            0.05,
+            0.95,
+            "Distance = {2:.0f} mm\n$\Delta t$ = {0:.0f} fs\n$\Delta \omega \\times \Delta t$ = {1:0.2f}".format(
+                self.temporalWidth * 1e15,
+                self.timeBandwidthProduct,
+                self.distancePropagated * 1e3,
+            ),
+            transform=axis.transAxes,
+            fontsize=14,
+            verticalalignment="top",
+        )
+
+    def endPulse(self, filenameTemplate):
+        if filenameTemplate is not None:
+            plt.savefig(filenameTemplate.format(j), dpi=300)
+
+    def draw(self, axis=None, showChirp=True, showCarrier=False, adjustTimeScale=True, filenameTemplate=None):
+        self.beginPulse(axis)
+        self.drawEnvelope()
+        self.drawChirpColour()
+    
+        if showCarrier:
+            self.drawField()
+            plt.ylim(-1,1)
+
+        if adjustTimeScale:
+            𝛕 = self.pulse.temporalWidth*1e12
+            plt.xlim(-5*𝛕, 5*𝛕)
+        
+        plt.draw()
+        plt.pause(0.001)
+        self.endPulse(filenameTemplate)
+
+    def drawEnvelope(self, axis=None):
+        if axis is None:
+            axis = plt.gca()
+
+        timeIsPs = self.pulse.time * 1e12
+        axis.plot(timeIsPs, self.pulse.fieldEnvelope, "k-")
+
+    def drawField(self, axis=None):
+        if axis is None:
+            axis = plt.gca()
+
+        (
+            instantTime,
+            instantEnvelope,
+            instantPhase,
+            instantRadFrequency,
+        ) = self.pulse.instantRadFrequency()
+
+        timeIsPs = instantTime * 1e12
+        axis.plot(timeIsPs, instantEnvelope * np.cos(instantPhase), "k-")
+
+    def drawChirpColour(self, axis=None):
+        if axis is None:
+            axis = plt.gca()
+
+        (
+            instantTime,
+            instantEnvelope,
+            instantPhase,
+            instantRadFrequency,
+        ) = self.pulse.instantRadFrequency()
+
+        # We want green for the center frequency (+0.33)
+        normalizedFrequencyForColor = (instantRadFrequency - self.pulse.𝝎ₒ) / (
+            5 * 2 * π * self.pulse.spectralWidth
+        ) + 0.33
+
+        hsv = cm.get_cmap("hsv", 64)
+        M = 128
+
+        instantTimeInPs = instantTime * 1e12
+        step = len(instantTimeInPs) // M
+        for i in range(0, len(instantTimeInPs) - step, step):
+            t1 = instantTimeInPs[i]
+            t2 = instantTimeInPs[i + step]
+            c = normalizedFrequencyForColor[i + step // 2]
+            e1 = instantEnvelope[i]
+            e2 = instantEnvelope[i + step]
+            axis.add_patch(
+                Polygon([(t1, 0), (t1, e1), (t2, e2), (t2, 0)], facecolor=hsv(c))
+            )
 
 if __name__ == "__main__":
 
 
     # All adjustable parameters below
-    pulse = Pulse(𝛕=5e-15, 𝜆ₒ=800e-9)
+    pulse = Pulse(𝛕=100e-15, 𝜆ₒ=800e-9)
 
     # Material propertiues and distances, steps
     material = pulse.bk7
-    totalDistance = 1e-2
-    steps = 40
+    totalDistance = 0.3
+    steps = 20
 
     # What to display on graph in addition to envelope?
     adjustTimeScale = False
     showCarrier = True
-
+    showChirpColour = True
     # Save graph? (set to None to not save)
     filenameTemplate = "fig-{0:02d}.png" # Can use PDF but PNG for making movies with Quicktime Player
 
     # End adjustable parameters
+
+    viewer = Viewer(pulse, "Propagation in {0}".format(material.__func__.__name__))
+    viewer.beginPlot()
 
     print("#\td[mm]\t∆t[ps]\t∆𝝎[THz]\tProduct")
     stepDistance = totalDistance / steps
@@ -293,22 +455,8 @@ if __name__ == "__main__":
             )
         )
 
-        pulse.setupPlot("Propagation in {0}".format(material.__func__.__name__))
-        pulse.drawEnvelope()
-        pulse.drawChirpColour()
-    
-        if showCarrier:
-            pulse.drawField()
-
-        if adjustTimeScale:
-            𝛕 = pulse.temporalWidth*1e12
-            plt.xlim(-5*𝛕, 5*𝛕)
-        
-        plt.draw()
-        plt.pause(0.001)
-
-        if filenameTemplate is not None:
-            plt.savefig(filenameTemplate.format(j), dpi=300)
-        pulse.tearDownPlot()
-
+        viewer.draw(showChirpColour, showCarrier, adjustTimeScale, filenameTemplate)
         pulse.propagate(stepDistance, material)
+
+    viewer.endPlot()
+
